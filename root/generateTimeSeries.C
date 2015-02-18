d02310a73ef66ed5d325d57304f601d51e8afcc2
Christoph Paus <paus@mit.edu>
2015-02-18 13:43:24 -0500

    Updating the time series.

--- root/generateTimeSeries.C
+++ root/generateTimeSeries.C
@@ -1,3 +1,9 @@
+#include <TRandom.h>
+#include <TCanvas.h>
+#include <TH1D.h>
+
+//#include <MitRootStyle.C>
+
 // Random generator available as global
 TRandom *gRandom = 0;
 
@@ -12,18 +18,17 @@ void  fillDecayTimes(const Int_t nDecays, Double_t decayTimes[]);
 // is a beta decay (e-) into Yttrium-90. The implementation is done such that it assumes a really
 // constant rate.
 //
-// The function makes a histogram of the observed counts and will fit the distribution to a Gaussian
-// which it of course is not but usually does a pretty decent job at it. One of the interesting
-// things to study here is to see how many experiments you need to perfrom to show a significant
-// deviation from Gaussian behavior. Quite a lot actually!
+// The function first determines how many radiocative decays it really recorded in the given time
+// interval at the specificed average rate and then generates a times series recording the time for
+// for each of the decays observed. From the time series we can first see whether the rate is
+// actually statistically speaking constant during the interval. We can also record the time
+// differences between two events to derive from the plot the meaning of average time difference
+// between events and the most likely difference (median).
 // --------------------------------------------------------------------------------------------------
 
-void generateTimeSeries(Int_t    seed     = 456456,
-			Int_t    nDraws   = 10000,
-		        Double_t rate     = 100,
-		        Double_t interval = 100,
-			Double_t mean     = 3400,
-			Double_t width    = 150,
+void generateTimeSeries(Int_t    seed     = 46456,
+		        Double_t rate     = 10,
+		        Double_t interval = 1000,
 			Double_t nBins    = 100)
 {
   // frame work where we setup the experiment and run in the measuring loop
@@ -37,29 +42,38 @@ void generateTimeSeries(Int_t    seed     = 456456,
   // book and generate the specific decay times 
   Double_t decayTimes[nObserved];
   fillDecayTimes(nObserved,decayTimes);
-
+  
   // analyze the time series (it is ordered already)
   TH1D *hTimes  = new TH1D("DecayT",";Number of Counts;Time [sec]",nBins,0.,interval);
   TH1D *hDeltaT = new TH1D("DeltaT",";Number of Entries;Delta Time [sec]",nBins,0.,5.0*(1.0/rate));
 
   // generate
   Double_t lastTime = 0.;
+
+  Double_t tTotal = 0;
   for (Int_t i=0; i<nObserved; i++) {
     Double_t deltaT = decayTimes[i] - lastTime;
 
     // book relevant histograms
     hTimes->Fill(decayTimes[i] * interval);
-    hDeltaT->Fill(deltaT);
+    hDeltaT->Fill(deltaT * interval);
 
+    tTotal += deltaT * interval;
+    
     // make sure to update the lastTime
     lastTime = decayTimes[i];
   }
-
+  
+  
+  printf(" Total time is: %f\n",tTotal);
+  printf(" Rate is      : %f\n",nObserved/interval);
+  
   // draw the plot
   TCanvas *cv = new TCanvas();
   cv->Draw();
-  hTimes->Draw("e");
+  //hTimes->Draw("e");
   hDeltaT->Draw("e");
+  hDeltaT->Fit("expo");
 
   // cleanup
   if (gRandom)
@@ -96,7 +110,7 @@ void fillDecayTimes(const Int_t nDecays, Double_t decayTimes[])
   }
   
   // allright, sorting it by hand
-  Int_t    idxLast =   -1;
+  // Int_t    idxLast =   -1;
   Double_t lowest  =  1.1;
   Double_t last    = -0.1;
   
@@ -106,7 +120,7 @@ void fillDecayTimes(const Int_t nDecays, Double_t decayTimes[])
     lowest = 1.1;             // reset our lowest to high value
     for (Int_t j=0; j<nDecays; j++) {
       if (x[j] < lowest && x[j] > last) {
-	idxLast = j;
+	//idxLast = j;
 	lowest  = x[j];
       }
     }
